ddb14ebb5139ec62e746acc5d48ae6626f3c05de
unknown <dudahpereira13@gmail.com>
2026-05-19 21:55:14 -0300

    model

--- nsei/src/main/java/com/example/nsei/models/ClienteModel.java
+++ nsei/src/main/java/com/example/nsei/models/ClienteModel.java
@@ -1,4 +1,56 @@
 package com.example.nsei.models;
 
-public class ClienteModel {
+import com.example.nsei.models.PedidoModel;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class ClienteModel{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<PedidoModel> pedidos;
+
+    public ClienteModel() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<PedidoModel> getPedidos() {
+        return pedidos;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPedidos(List<PedidoModel> pedidos) {
+        this.pedidos = pedidos;
+    }
 }
--- nsei/src/main/java/com/example/nsei/models/PedidoModel.java
+++ nsei/src/main/java/com/example/nsei/models/PedidoModel.java
@@ -1,4 +1,45 @@
 package com.example.nsei.models;
 
+import com.example.nsei.models.ProdutoModel;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
 public class PedidoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String cliente;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<ProdutoModel> products;
+
+    public PedidoModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public List<ProdutoModel> getProducts() {
+        return products;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public void setProducts(List<ProdutoModel> products) {
+        this.products = products;
+    }
 }
