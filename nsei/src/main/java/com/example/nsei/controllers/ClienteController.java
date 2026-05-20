bab2acf052aa7b156d23c7b98d9f8aed0fe479b4
unknown <dudahpereira13@gmail.com>
2026-05-19 22:22:48 -0300

    services corrigidos

--- nsei/src/main/java/com/example/nsei/controllers/ClienteController.java
+++ nsei/src/main/java/com/example/nsei/controllers/ClienteController.java
@@ -1,4 +1,55 @@
 package com.example.nsei.controllers;
 
+
+import com.example.nsei.models.ClienteModel;
+import com.example.nsei.services.ClienteService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+
+    private final ClienteService service;
+
+    public ClienteController(ClienteService service) {
+        this.service = service;
+    }
+
+    // Criar cliente
+    @PostMapping
+    public ClienteModel salvar(@RequestBody ClienteModel cliente) {
+        return service.salvar(cliente);
+    }
+
+    // Listar todos clientes
+    @GetMapping
+    public List<ClienteModel> listarTodos() {
+        return service.listarTodos();
+    }
+
+    // Buscar cliente por ID
+    @GetMapping("/{id}")
+    public Cliente buscarPorId(@PathVariable Long id) {
+
+        return service.buscarPorId(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Cliente não encontrado"));
+    }
+
+    // Atualizar cliente
+    @PutMapping("/{id}")
+    public Cliente atualizar(
+            @PathVariable Long id,
+            @RequestBody Cliente cliente) {
+
+        return service.atualizar(id, cliente);
+    }
+
+    // Deletar cliente
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
 }
--- nsei/src/main/java/com/example/nsei/services/ClienteService.java
+++ nsei/src/main/java/com/example/nsei/services/ClienteService.java
@@ -1,4 +1,52 @@
 package com.example.nsei.services;
 
+
+import com.example.nsei.models.ClienteModel;
+import com.example.nsei.repositories.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ClienteService {
+
+    private final ClienteRepository repository;
+
+    public ClienteService(ClienteRepository repository) {
+        this.repository = repository;
+    }
+
+    public ClienteModel salvar(ClienteModel cliente) {
+        return repository.save(cliente);
+    }
+
+    public List<ClienteModel> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<ClienteModel> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public ClienteModel atualizar(Long id, ClienteModel clienteAtualizado) {
+
+        ClienteModel cliente = repository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Cliente não encontrado"));
+
+        cliente.setNome(clienteAtualizado.getNome());
+        cliente.setEmail(clienteAtualizado.getEmail());
+
+        return repository.save(cliente);
+    }
+
+    public void deletar(Long id) {
+
+        ClienteModel cliente = repository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Cliente não encontrado"));
+
+        repository.delete(cliente);
+    }
 }
